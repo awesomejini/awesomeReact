fcfbd997b425230c3c1722a4549b0e02b683e5ed
awesomejini <75153322+awesomejini@users.noreply.github.com>
2021-05-13 11:32:49 +0900

    Add files via upload

---
+++ etc-practice/App.js
@@ -0,0 +1,12 @@
+import ScrollBox from "./ScrollBox";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ScrollBox />
+    </div>
+  );
+}
+
+export default App;
