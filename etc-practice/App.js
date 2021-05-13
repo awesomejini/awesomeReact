c1ffff19e6d444eadfa5c042660d1836cf03fee3
awesomejini <75153322+awesomejini@users.noreply.github.com>
2021-05-13 11:35:31 +0900

    20210513_Practice
    component 에 ref 쓰기

--- etc-practice/App.js
+++ etc-practice/App.js
@@ -1,12 +1,18 @@
 import ScrollBox from "./ScrollBox";
 import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <ScrollBox />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
